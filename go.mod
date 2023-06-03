cb613d22958d9f611bb6df1c3e4e7e43c38f410e
kfrico <kfrico@gmail.com>
2023-06-03 14:19:18 +0800

    init

--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.20
 require (
 	github.com/asticode/go-astikit v0.40.0
 	github.com/asticode/go-astilectron v0.30.0
+	github.com/asticode/go-astilectron-bootstrap v0.4.14
 	github.com/getlantern/systray v1.2.2
 )
 
 require (
 	github.com/akavel/rsrc v0.10.2 // indirect
-	github.com/asticode/go-astilectron-bootstrap v0.4.14 // indirect
 	github.com/asticode/go-astilectron-bundler v0.7.12 // indirect
 	github.com/asticode/go-bindata v1.0.0 // indirect
 	github.com/getlantern/context v0.0.0-20220418194847-3d5e7a086201 // indirect
--- main.go
+++ main.go
@@ -1,173 +1,169 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
-	astikit "github.com/asticode/go-astikit"
-	astilectron "github.com/asticode/go-astilectron"
+	"github.com/asticode/go-astikit"
+	"github.com/asticode/go-astilectron"
+	bootstrap "github.com/asticode/go-astilectron-bootstrap"
 )
 
+// Constants
+const htmlAbout = `Welcome on <b>Astilectron</b> demo!<br>
+This is using the bootstrap and the bundler.`
+
+// Vars injected via ldflags by bundler
 var (
 	AppName            string
 	BuiltAt            string
 	VersionAstilectron string
 	VersionElectron    string
+)
 
-	w *astilectron.Window
+// Application Vars
+var (
+	fs    = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	debug = fs.Bool("d", false, "enables the debug mode")
+	w     *astilectron.Window
 )
 
 func main() {
-	// Set logger
+	// Create logger
 	l := log.New(log.Writer(), log.Prefix(), log.Flags())
 
-	// Create astilectron
-	a, err := astilectron.New(l, astilectron.Options{
-		AppName:            "Chat",
-		AppIconDarwinPath:  "resources/icon.icns",
-		AppIconDefaultPath: "resources/icon.png",
-		SingleInstance:     true,
-		VersionAstilectron: VersionAstilectron,
-		VersionElectron:    VersionElectron,
-	})
+	// Parse flags
+	fs.Parse(os.Args[1:])
 
-	if err != nil {
-		l.Fatal(fmt.Errorf("main: creating astilectron failed: %w", err))
-	}
-	defer a.Close()
+	// Run bootstrap
+	l.Printf("Running app built at %s\n", BuiltAt)
 
-	// Handle signals
-	a.HandleSignals()
-
-	// Start
-	if err = a.Start(); err != nil {
-		l.Fatal(fmt.Errorf("main: starting astilectron failed: %w", err))
-	}
-
-	// Init a new app menu
-	var m = a.NewMenu([]*astilectron.MenuItemOptions{
-		{
-			Label: astikit.StrPtr("Chat"),
-			SubMenu: []*astilectron.MenuItemOptions{
-				{Label: astikit.StrPtr("Minimize"), Role: astilectron.MenuItemRoleMinimize},
-				{Label: astikit.StrPtr("Close"), Role: astilectron.MenuItemRoleClose},
-				{Label: astikit.StrPtr("About"), Role: astilectron.MenuItemRoleAbout},
+	err := bootstrap.Run(bootstrap.Options{
+		Asset:    Asset,
+		AssetDir: AssetDir,
+		AstilectronOptions: astilectron.Options{
+			AppName:            "Chat",
+			AppIconDarwinPath:  "resources/icon.icns",
+			AppIconDefaultPath: "resources/icon.png",
+			SingleInstance:     true,
+			VersionAstilectron: VersionAstilectron,
+			VersionElectron:    VersionElectron,
+		},
+		Debug:  *debug,
+		Logger: l,
+		MenuOptions: []*astilectron.MenuItemOptions{
+			{
+				Label: astikit.StrPtr("Chat"),
+				SubMenu: []*astilectron.MenuItemOptions{
+					{Label: astikit.StrPtr("Minimize"), Role: astilectron.MenuItemRoleMinimize},
+					{Accelerator: astilectron.NewAccelerator("Command", "Q"), Label: astikit.StrPtr("Close"), Role: astilectron.MenuItemRoleClose},
+					{Accelerator: astilectron.NewAccelerator("Command", "R"), Label: astikit.StrPtr("Reload"), Role: astilectron.MenuItemRoleReload},
+					{Label: astikit.StrPtr("About"), Role: astilectron.MenuItemRoleAbout},
+				},
 			},
+		},
+
+		RestoreAssets: RestoreAssets,
+		TrayOptions: &astilectron.TrayOptions{
+			Image:   astikit.StrPtr("resources/icon200.png"),
+			Tooltip: astikit.StrPtr("tooltip"),
+		},
+		Windows: []*bootstrap.Window{
+			{
+				Homepage: "https://mail.google.com/chat",
+				Options: &astilectron.WindowOptions{
+					Center:    astikit.BoolPtr(true),
+					MinHeight: astikit.IntPtr(700),
+					MinWidth:  astikit.IntPtr(500),
+					Height:    astikit.IntPtr(700),
+					Width:     astikit.IntPtr(700),
+				},
+			},
+		},
+
+		OnWait: func(a *astilectron.Astilectron, ws []*astilectron.Window, _ *astilectron.Menu, t *astilectron.Tray, _ *astilectron.Menu) error {
+			basePaht := a.Paths().BaseDirectory()
+
+			ws[0].OnMessage(func(m *astilectron.EventMessage) interface{} {
+				// Unmarshal
+				var count int
+				m.Unmarshal(&count)
+
+				switch count {
+				case 0:
+					t.SetImage(basePaht + "/resources/icon200.png")
+				case 1:
+					t.SetImage(basePaht + "/resources/icon201.png")
+				case 2:
+					t.SetImage(basePaht + "/resources/icon202.png")
+				case 3:
+					t.SetImage(basePaht + "/resources/icon203.png")
+				case 4:
+					t.SetImage(basePaht + "/resources/icon204.png")
+				case 5:
+					t.SetImage(basePaht + "/resources/icon205.png")
+				case 6:
+					t.SetImage(basePaht + "/resources/icon206.png")
+				case 7:
+					t.SetImage(basePaht + "/resources/icon207.png")
+				case 8:
+					t.SetImage(basePaht + "/resources/icon208.png")
+				case 9:
+					t.SetImage(basePaht + "/resources/icon209.png")
+				default:
+					t.SetImage(basePaht + "/resources/icon20o.png")
+
+				}
+
+				return nil
+			})
+
+			ws[0].ExecuteJavaScript(`
+          // This will send a message to GO
+          astilectron.sendMessage("hello", function(message) {
+              console.log("received " + message)
+          });
+
+          const aktDoms = document.getElementsByClassName('akt');
+          let count = 0;
+
+          function computeCount() {
+            count = 0;
+
+            for (let i = 0; i < aktDoms.length; i++) {
+              const span = aktDoms[i].getElementsByTagName('span')[0];
+
+              count += Number(span.textContent);
+            }
+
+            const link = document.createElement('A');
+
+              astilectron.sendMessage(count, function() {
+
+              });
+          }
+
+          for (let i = 0; i < aktDoms.length; i++) {
+            aktDoms[i].addEventListener('DOMSubtreeModified', () => {
+              computeCount();
+            });
+          }
+
+          computeCount();
+      `)
+
+			t.On(astilectron.EventNameTrayEventClicked, func(e astilectron.Event) (deleteListener bool) {
+
+				return
+			})
+
+			return nil
 		},
 	})
 
-	m.Create()
-
-	// New window
-	var w *astilectron.Window
-	if w, err = a.NewWindow("https://mail.google.com/chat", &astilectron.WindowOptions{
-		WebPreferences: &astilectron.WebPreferences{},
-		Center:         astikit.BoolPtr(true),
-		MinHeight:      astikit.IntPtr(700),
-		MinWidth:       astikit.IntPtr(500),
-		Height:         astikit.IntPtr(700),
-		Width:          astikit.IntPtr(700),
-	}); err != nil {
-		l.Fatal(fmt.Errorf("main: new window failed: %w", err))
+	if err != nil {
+		l.Fatal(fmt.Errorf("running bootstrap failed: %w", err))
 	}
-
-	// Create windows
-	if err = w.Create(); err != nil {
-		l.Fatal(fmt.Errorf("main: creating window failed: %w", err))
-	}
-
-	var t = a.NewTray(&astilectron.TrayOptions{
-		Image:   astikit.StrPtr("resources/icon.png"),
-		Tooltip: astikit.StrPtr("tooltip"),
-	})
-
-	// Create tray
-	t.Create()
-
-	t.On(astilectron.EventNameTrayEventClicked, func(e astilectron.Event) (deleteListener bool) {
-		// err = w.Focus()
-		// if err != nil {
-		// 	fmt.Println("err###############", err)
-		// }
-		// err = w.Show()
-		// if err != nil {
-		// 	fmt.Println("err###############", err)
-		// }
-		// fmt.Println("Tray item clicked")
-		return
-	})
-
-	w.OnMessage(func(m *astilectron.EventMessage) interface{} {
-		// Unmarshal
-		var count int
-		m.Unmarshal(&count)
-
-		switch count {
-		case 0:
-			t.SetImage("resources/icon200.png")
-		case 1:
-			t.SetImage("resources/icon201.png")
-		case 2:
-			t.SetImage("resources/icon202.png")
-		case 3:
-			t.SetImage("resources/icon203.png")
-		case 4:
-			t.SetImage("resources/icon204.png")
-		case 5:
-			t.SetImage("resources/icon205.png")
-		case 6:
-			t.SetImage("resources/icon206.png")
-		case 7:
-			t.SetImage("resources/icon207.png")
-		case 8:
-			t.SetImage("resources/icon208.png")
-		case 9:
-			t.SetImage("resources/icon209.png")
-		default:
-			t.SetImage("resources/icon20o.png")
-
-		}
-
-		return nil
-	})
-
-	// 開發者工具
-	// w.OpenDevTools()
-
-	// 注入監聽未讀訊息的Js
-	w.ExecuteJavaScript(`
-		    // This will send a message to GO
-		    astilectron.sendMessage("hello", function(message) {
-		        console.log("received " + message)
-		    });
-
-		    const aktDoms = document.getElementsByClassName('akt');
-		    let count = 0;
-
-		    function computeCount() {
-		      count = 0;
-
-		      for (let i = 0; i < aktDoms.length; i++) {
-		        const span = aktDoms[i].getElementsByTagName('span')[0];
-
-		        count += Number(span.textContent);
-		      }
-
-		      const link = document.createElement('A');
-
-			    astilectron.sendMessage(count, function() {
-
-			    });
-		    }
-
-		    for (let i = 0; i < aktDoms.length; i++) {
-		      aktDoms[i].addEventListener('DOMSubtreeModified', () => {
-		        computeCount();
-		      });
-		    }
-
-		    computeCount();
-		`)
-
-	a.Wait()
 }
